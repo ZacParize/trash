5203805e5da85d380da9e25b5f051a14d66d725e
ZacParize <ZacParize@users.noreply.github.com>
2024-07-29 11:11:45 +0300

    Add files via upload
    init commit

---
+++ simple_web_server.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+from urllib.parse import urlparse, parse_qs, unquote
+
+hostName = "localhost"
+serverPort = 8111
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        query_params = dict(parse_qs(urlparse(unquote(self.path)).query))
+        print(query_params)
+        page = int(query_params['page'][0])
+        if page <= 100:
+            self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+        else:
+            self.wfile.write(bytes("", "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
